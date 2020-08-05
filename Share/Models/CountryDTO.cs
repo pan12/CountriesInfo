eef6024754b4f0ec488e6a7cb88879ff1a84b603
pan12 <6729656+pan12@users.noreply.github.com>
2020-08-05 22:49:31 +0400

    Add Equals method

--- Share/Models/CountryDTO.cs
+++ Share/Models/CountryDTO.cs
@@ -12,5 +12,18 @@ namespace Share.Models
         public double Area { get; set; }
         public string Capital { get; set; }
         public string Region { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (this.Area == ((CountryDTO)obj).Area &&
+                this.Capital == ((CountryDTO)obj).Capital &&
+                this.Code == ((CountryDTO)obj).Code &&
+                this.Name == ((CountryDTO)obj).Name &&
+                this.Population == ((CountryDTO)obj).Population &&
+                this.Region == ((CountryDTO)obj).Region)
+            { return true; }
+            else
+            { return false; }
+        }
     }
 }
